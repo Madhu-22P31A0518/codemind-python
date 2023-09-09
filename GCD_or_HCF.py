a91aaf589487edfc586324aceeaf0b4f076988b4
Madhu-22P31A0518 <119848350+Madhu-22P31A0518@users.noreply.github.com>
2023-09-09 20:11:19 +0530

    a,b=map(int,input().split()) e=[] q=[] c=[] for i in range(1,b*b):     if a%i==0:         e.append(i)     if b%i==0:         c.append(i) for j in e:     if j in c:         q.append(j) print(max(q))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,14 +1,13 @@
 a,b=map(int,input().split())
 e=[]
 q=[]
-s=[]
-for i in range(1,a+1):
+c=[]
+for i in range(1,b*b):
     if a%i==0:
         e.append(i)
-for j in range(1,b+1):
-    if b%j==0:
+    if b%i==0:
+        c.append(i)
+for j in e:
+    if j in c:
         q.append(j)
-for k in e:
-    if k in q:
-        s.append(k)
-print(max(s))
+print(max(q))
